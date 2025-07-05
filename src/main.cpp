864c36b1d3c0fbccfc2fddd1fe414c90c1504303
rakshitraj <rakshit3181@gmail.com>
2025-07-05 18:03:09 +0530

    use switch case and enums to parse commands

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@
 #include "include/init.hpp"
 #include "include/cat-file.hpp"
 
+enum class Command { // Use CamelCase
+    Init,
+    CatFile,
+    Unknown
+};
+
+Command parse_command(const std::string& cmd) {
+    if (cmd == "init") return Command::Init;
+    if (cmd == "cat-file") return Command::CatFile;
+    return Command::Unknown;
+}
+
 int main(int argc, char *argv[])
 {
     // Flush after every std::cout / std::cerr
@@ -21,14 +33,15 @@ int main(int argc, char *argv[])
     }
     
     std::string command = argv[1];
-    
-    if (command == "init") {
-        return git_init();
-    } else if (command == "cat-file") {
-        return cat_file(argc, argv);
-    } else {
-        std::cerr << "Unknown command " << command << '\n';
-        return EXIT_FAILURE;
+
+    switch(parse_command(command)) {
+        case Command::Init:
+            return git_init();
+        case Command::CatFile:
+            return cat_file(argc, argv);
+        case Command::Unknown:
+            std::cout << "Unknown command " << command << '\n';
+            return EXIT_FAILURE;
     }
     
     return EXIT_SUCCESS;
