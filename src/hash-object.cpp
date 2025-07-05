36ae9ce68e7b12c3f6f7498ab90365629b9b04f9
rakshitraj <rakshit3181@gmail.com>
2025-07-05 23:16:14 +0530

    stage 3: hash-object, fixes

--- src/hash-object.cpp
+++ src/hash-object.cpp
@@ -29,7 +29,6 @@ void compress_zlib(std::vector<char>& uncompressed_data, std::vector<char>& out)
 
     // Init Deflate
     if (deflateInit(&strm, Z_BEST_SPEED) != Z_OK) {
-        std::cout << "err1\n";
         throw std::runtime_error("deflateInit failed");
     }
     // Deflate uncompressed data
@@ -40,14 +39,12 @@ void compress_zlib(std::vector<char>& uncompressed_data, std::vector<char>& out)
         strm.avail_out = CHUNK_SIZE;
         ret = deflate(&strm, Z_FINISH);
         if (ret == Z_STREAM_ERROR) {
-            std::cout << "err2\n";
             throw std::runtime_error("deflate failed in compress_zlib");
         }
         out.insert(out.end(), buffer, buffer + (CHUNK_SIZE - strm.avail_out));
     } while (strm.avail_out == 0);
     // End deflate
     if (deflateEnd(&strm) != Z_OK) {
-        std::cout << "err3\n";
         throw std::runtime_error("deflateEnd failed");
     }
 }
@@ -88,15 +85,9 @@ int hash_object(const int argc, const char* const argv[]) {
     std::string header = type + " " + std::to_string(file_contents.size()) + '\0';
     file_contents.insert(file_contents.begin(), header.begin(), header.end());
 
-    std::cout << "hashing...\n";
     std::string hash = sha1_hash(file_contents);
-    std::cout << "hash --> " << hash << '\n';
-
-    std::cout << "serailizing...\n";
     std::string out = serialize(file_contents);
-    std::cout << "out --> " << out << '\n';
 
-    std::cout << "Writing object...\n";
     if (flag == "-w") {
         std::string object_dir = hash.substr(0,2);
         std::string object_name = hash.substr(2);
